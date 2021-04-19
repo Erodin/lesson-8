bd87d5ab70b2568786d377016fa3193a46cd52cc
Erodin <ermin@doc.ba>
2021-04-19 15:45:22 +0200

    moved our directory and shop data into own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,7 +12,7 @@ export const selectCartItemsCount = createSelector([selectCartItems], (cartItems
 
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity + cartItem.price,
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price,
     0,
   ),
 );
